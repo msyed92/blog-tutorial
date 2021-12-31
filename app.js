d62690a5d743c7f16667ea72d33a7f49af66e814
Manaal Syed <msyed@wpi.edu>
2021-12-31 11:57:50 -0500

    update blog with mongodb

--- app.js
+++ app.js
@@ -3,26 +3,64 @@
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
-// Load the full build.
-const _ = require('lodash')
+const mongoose = require("mongoose")
+const _ = require("lodash")
+const app = express();
+
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
+app.locals._ = _
+
+const URI = ""
+mongoose.connect(
+  URI,
+  { useNewUrlParser: true })
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+})
+const Post = mongoose.model("Post", postSchema)
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
-const app = express();
+const homePost = new Post({
+  title: "home",
+  content: homeStartingContent
+})
 
-let posts = []
+const aboutPost = new Post({
+  title: "about",
+  content: aboutContent
+})
 
-app.set('view engine', 'ejs');
+const contactPost = new Post({
+  title: "contact",
+  content: contactContent
+})
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+let defaultPosts = [homePost, aboutPost, contactPost]
 
 app.get("/", (req, res) => {
-  res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+  Post.find({}, (err, foundPosts) => {
+    if (foundPosts.length == 0) {
+      Post.insertMany(defaultPosts, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("posts added")
+        }
+      })
+      res.redirect("/")
+    } else {
+      console.log(foundPosts)
+      res.render("home", { posts: foundPosts });
+    }
+
+
   })
 })
 
@@ -35,25 +73,36 @@ app.get("/contact", (req, res) => {
 })
 
 app.get("/compose", (req, res) => {
-  res.render("compose", { startingContent: contactContent })
+  res.render("compose")
 })
 
-app.get("/posts/:title", (req, res) => {
-  const url = _.lowerCase(req.params.title)
-  const post = posts.find(post => _.lowerCase(post.title) == url)
-  res.render("post", { title: post.title, content: post.content })
+app.get("/posts/:postID", (req, res) => {
+  const postID = req.params.postID
+  Post.findOne({ _id: postID }, (err, post) => {
+    if (err) {
+      console.log(err)
+    } else {
+      if (post) {
+        res.render("post", { title: _.startCase(post.title), content: post.content })
+      } else {
+        res.render("compose")
+      }
+    }
+  })
 })
 
 app.post("/compose", (req, res) => {
-  let urlTitle = _.lowerCase(req.body.postTitle)
-  urlTitle = _.replace(urlTitle, " ", "-")
-  const post = {
-    title: req.body.postTitle,
-    urlTitle: urlTitle,
+  const post = new Post({
+    title: _.lowerCase(req.body.postTitle),
     content: req.body.postBody
-  }
-  posts.push(post)
-  res.redirect("/")
+  })
+  post.save((err, post) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.redirect("/")
+    }
+  })
 })
 
 app.listen(3000, function () {
